58ded411b84f0303089fba33e8f803e84493a2aa
Tambet Masik <tambet@toggl.com>
2013-06-19 14:41:59 +0300

    NotifyRequest sets http.Request body as event meta-data

--- bugsnag.go
+++ bugsnag.go
@@ -122,10 +122,19 @@ func Notify(err error) error {
 	return New(err).Notify()
 }
 
-// NotifyRequest sends an error to bugsnag, and sets request
-// URL as the event context.
+// NotifyRequest sends an error to bugsnag, sets request
+// URL as the event context and request body as event meta-data
 func NotifyRequest(err error, r *http.Request) error {
-	return New(err).WithContext(r.URL.String()).Notify()
+	n := New(err).WithContext(r.URL.String())
+	if r.Body != nil {
+		defer r.Body.Close()
+		if b, err := ioutil.ReadAll(r.Body); err == nil {
+			if len(b) > 0 {
+				n.WithMetaData("params", "body", string(b))
+			}
+		}
+	}
+	return n.Notify()
 }
 
 // New returns returns a bugsnag event instance, that can be further configured
